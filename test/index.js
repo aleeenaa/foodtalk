59012e1d81dac18cc4949a0c3d770a6d3af391a5
barthola <up716038@myport.ac.uk>
2017-04-28 15:41:14 +0100

    Added tests for UJ2.

--- test/index.js
+++ test/index.js
@@ -22,7 +22,16 @@ describe('Express Server: Foodtalk Fulfilment API', () => {
 			})
 		})
 	})
-
+/**
+ * [description]
+ * @param  {[type]}		UJ1		         User Journey 1
+ * @param  {Function}	Test case definition of UJ1
+ * @param  {[type]}		{
+ *         				'content-type': 'application/json'
+ *         				}				Header
+ * @param  {Function} (err,res,body)	Response Objects
+ * @return {[type]}						Results for Assertion
+ */
 	describe('UJ1', () => {
 		it('Invocation Trigger: foodtalk', (done) => {
 			request({
@@ -122,11 +131,21 @@ describe('Express Server: Foodtalk Fulfilment API', () => {
 			})
 		})
 	})
-
-	describe('UJ1', () => {
-		it('Invocation Trigger: foodtalk', (done) => {
+/**
+ * [description]
+ * @param  {[type]}		UJ2'			User Journey 2
+ * @param  {Function}	Test case definition of UJ2: Happy
+ *                      Ending with Two Rounds
+ * @param  {[type]}		{
+ *         				'content-type': 'application/json'
+ *         				}				Header
+ * @param  {Function} (err,res,body)	Response Objects
+ * @return {[type]}						Results for Assertion
+ */
+	describe('UJ2', () => {
+		it('Invocation Trigger: Let me talk to food talk please', (done) => {
 			request({
-				url: 'https://api.api.ai/api/query?v=20150910&query=foodtalk&lang=en&sessionId=832fba30-130f-49cd-8244-af5a8c5959ce&timezone=2017-04-27T21:17:53+0100',
+				url: 'https://api.api.ai/api/query?v=20150910&query=Let%20me%20talk%20to%20Foodtalk%20please!&lang=en&sessionId=832fba30-130f-49cd-8244-af5a8c5959ce&timezone=2017-04-28T14:59:03+0100',
 				headers: {
 					'Authorization':`Bearer ${accessToken}`,
 					'content-type': 'application/json'
@@ -138,9 +157,9 @@ describe('Express Server: Foodtalk Fulfilment API', () => {
 			})
 		})
 
-		it('A lamb wrap please', (done) => {
+		it('Chicken burger please', (done) => {
 			request({
-				url: 'https://api.api.ai/api/query?v=20150910&query=lamb%20wrap%20please&lang=en&sessionId=832fba30-130f-49cd-8244-af5a8c5959ce&timezone=2017-04-28T14:36:48+0100',
+				url: 'https://api.api.ai/api/query?v=20150910&query=Chicken%20burger%20please&lang=en&sessionId=832fba30-130f-49cd-8244-af5a8c5959ce&timezone=2017-04-28T15:03:48+0100',
 				headers: {
 					'Authorization':`Bearer ${accessToken}`,
 					'content-type': 'application/json'
@@ -152,9 +171,9 @@ describe('Express Server: Foodtalk Fulfilment API', () => {
 			})
 		})
 
-		it('Shake & Grill', (done) => {
+		it('From umami please', (done) => {
 			request({
-				url: 'https://api.api.ai/api/query?v=20150910&query=shake%20%26%20grill&lang=en&sessionId=832fba30-130f-49cd-8244-af5a8c5959ce&timezone=2017-04-28T14:50:50+0100',
+				url: 'https://api.api.ai/api/query?v=20150910&query=From%20umami%20please&lang=en&sessionId=832fba30-130f-49cd-8244-af5a8c5959ce&timezone=2017-04-28T15:04:35+0100',
 				headers: {
 					'Authorization':`Bearer ${accessToken}`,
 					'content-type': 'application/json'
@@ -166,9 +185,9 @@ describe('Express Server: Foodtalk Fulfilment API', () => {
 			})
 		})
 
-		it('A coke and fries please', (done) => {
+		it('Yeah a coke please', (done) => {
 			request({
-				url: 'https://api.api.ai/api/query?v=20150910&query=A%20coke%20and%20fries%20please&lang=en&sessionId=832fba30-130f-49cd-8244-af5a8c5959ce&timezone=2017-04-28T14:51:32+0100',
+				url: 'https://api.api.ai/api/query?v=20150910&query=Yeah%20a%20coke%20please&lang=en&sessionId=832fba30-130f-49cd-8244-af5a8c5959ce&timezone=2017-04-28T15:05:08+0100',
 				headers: {
 					'Authorization':`Bearer ${accessToken}`,
 					'content-type': 'application/json'
@@ -180,9 +199,9 @@ describe('Express Server: Foodtalk Fulfilment API', () => {
 			})
 		})
 
-		it('No thanks', (done) => {
+		it("Naa its good", (done) => {
 			request({
-				url: 'https://api.api.ai/api/query?v=20150910&query=No%20thanks&lang=en&sessionId=832fba30-130f-49cd-8244-af5a8c5959ce&timezone=2017-04-28T14:52:28+0100',
+				url: "https://api.api.ai/api/query?v=20150910&query=Naa%20it's%20good&lang=en&sessionId=832fba30-130f-49cd-8244-af5a8c5959ce&timezone=2017-04-28T15:05:41+0100",
 				headers: {
 					'Authorization':`Bearer ${accessToken}`,
 					'content-type': 'application/json'
@@ -208,9 +227,79 @@ describe('Express Server: Foodtalk Fulfilment API', () => {
 			})
 		})
 
-		it('Naa', (done) => {
+		it('Yeah I want chinese', (done) => {
 			request({
-				url: 'https://api.api.ai/api/query?v=20150910&query=naa&lang=en&sessionId=832fba30-130f-49cd-8244-af5a8c5959ce&timezone=2017-04-28T14:54:28+0100',
+				url: 'https://api.api.ai/api/query?v=20150910&query=Yeah%20I%20want%20chinese&lang=en&sessionId=832fba30-130f-49cd-8244-af5a8c5959ce&timezone=2017-04-28T15:19:49+0100',
+				headers: {
+					'Authorization':`Bearer ${accessToken}`,
+					'content-type': 'application/json'
+				}
+			},
+			(err, res, body) => {
+				assert.equal(200, res.statusCode);
+				done();
+			})
+		})
+
+		it('Lets go with china town', (done) => {
+			request({
+				url: 'https://api.api.ai/api/query?v=20150910&query=Lets%20go%20with%20china%20town&lang=en&sessionId=832fba30-130f-49cd-8244-af5a8c5959ce&timezone=2017-04-28T15:20:30+0100',
+				headers: {
+					'Authorization':`Bearer ${accessToken}`,
+					'content-type': 'application/json'
+				}
+			},
+			(err, res, body) => {
+				assert.equal(200, res.statusCode);
+				done();
+			})
+		})
+
+		it('I want chow mein', (done) => {
+			request({
+				url: 'https://api.api.ai/api/query?v=20150910&query=I%20want%20chow%20mein&lang=en&sessionId=832fba30-130f-49cd-8244-af5a8c5959ce&timezone=2017-04-28T15:21:14+0100',
+				headers: {
+					'Authorization':`Bearer ${accessToken}`,
+					'content-type': 'application/json'
+				}
+			},
+			(err, res, body) => {
+				assert.equal(200, res.statusCode);
+				done();
+			})
+		})
+
+		it("No that's fine", (done) => {
+			request({
+				url: "https://api.api.ai/api/query?v=20150910&query=No%20that's%20fine&lang=en&sessionId=832fba30-130f-49cd-8244-af5a8c5959ce&timezone=2017-04-28T15:22:04+0100",
+				headers: {
+					'Authorization':`Bearer ${accessToken}`,
+					'content-type': 'application/json'
+				}
+			},
+			(err, res, body) => {
+				assert.equal(200, res.statusCode);
+				done();
+			})
+		})
+
+		it('Yup', (done) => {
+			request({
+				url: 'https://api.api.ai/api/query?v=20150910&query=yup&lang=en&sessionId=832fba30-130f-49cd-8244-af5a8c5959ce&timezone=2017-04-28T14:53:55+0100',
+				headers: {
+					'Authorization':`Bearer ${accessToken}`,
+					'content-type': 'application/json'
+				}
+			},
+			(err, res, body) => {
+				assert.equal(200, res.statusCode);
+				done();
+			})
+		})
+
+		it('No thanks', (done) => {
+			request({
+				url: 'https://api.api.ai/api/query?v=20150910&query=no%20thanks&lang=en&sessionId=832fba30-130f-49cd-8244-af5a8c5959ce&timezone=2017-04-28T15:23:23+0100',
 				headers: {
 					'Authorization':`Bearer ${accessToken}`,
 					'content-type': 'application/json'
